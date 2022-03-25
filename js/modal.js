549a9a4d471586cfbe7027842f1c3337ca4936a6
VGrolleau <virginie.grolleau+github@protonmail.com>
2022-03-25 13:36:14 +0100

    Manage close method

--- js/modal.js
+++ js/modal.js
@@ -12,12 +12,21 @@ function editNav() {
 const modalbg = document.querySelector(".modal-bg");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+closeBtn.addEventListener('click', closeModal);
 
 // launch modal form
 function launchModal() {
     // modalbg.style.display = "block";
     modalbg.style.display = "flex";
+}
+
+// function close
+function closeModal() {
+    modalbg.style.display = "none";
 }
